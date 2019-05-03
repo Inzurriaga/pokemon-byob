0e4a7a7d1f1553941dee2a4f968e2c595b5f80de
Gabriel Inzurriaga <gabeinzurriaga@yahoo.com>
2019-05-03 10:12:39 -0600

    fix backend

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors())
 
 app.set('port', process.env.PORT || 3000).
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(app.get("port"), () => {
     console.log("Server is running on http://localhost:3000")
 })
 
